8e940105189bf72223016ae4835b23446008427b
brozyad <ziyadrsyd122@gmail.com>
2024-07-18 13:09:51 +0700

    update footer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,7 @@
 import Link from "next/link"
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
-import Layout from "../components/Layout"
 import Footer from "../components/Footer"
-import Image from "next/image"
-import Carousel from "../components/Carousel"
-import ServicesButton from "../components/Home/ServicesButton"
 import AboutUsHero from "../components/Home/AboutUsHero"
 import NewServicesHero from "../components/Home/NewServicesHero"
 
